896fcc60894e490d2c5695827b19f7ad47386947
Zasha <zahid.shaikh@live.com>
2024-01-18 18:04:08 +0530

    distDir removed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: 'build',
   reactStrictMode: false,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
