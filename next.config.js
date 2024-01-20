bdcf45b7b56720dcad0e8b6c9840ba8e988e3372
Zasha <zahid.shaikh@live.com>
2024-01-21 03:47:51 +0530

    corrected cors links

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return[
       {
         source: '/api/:path*',
-        destination: 'https:///localhost:3000/:path*',
+        destination: 'http://grimaceonsol.xyz/:path*',
       }
     ]
   },
